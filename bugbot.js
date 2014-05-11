038e907d032f65183aa8ef5af273f8d2662d1b55
Jim McGaw <jpmcgaw@gmail.com>
2014-05-11 11:25:42 -0700

    whisker refactor out of motor

--- bugbot.js
+++ bugbot.js
@@ -17,37 +17,35 @@ var RIGHT_MOTOR_2 = 22;
 var LEFT_MOTOR_1 = 7;
 var LEFT_MOTOR_2 = 15;
 
-// whisker component of a motor
+// manages and senses the state of a single whisker
 var Whisker = function(pins){
   this.pinIn = pins.pinIn;
   this.out = pins.out; 
 };
 
+Whisker.prototype.enable = function(){
+  console.log("enabling whisker pins " + this.pinIn + " and " + this.out);
+  gpio.open(this.pinIn, "input", function(){});
+  
+  var whisker_out = this.out;
+  gpio.open(whisker_out, "output", function(){
+    gpio.write(whisker_out, HIGH, function(){});
+  });
+
+};
+
 // this controls the state of a single servo motor
 //
-// 'pins' defines the following:
-// this.whisker_in
-// this.whisker_out
-// this.motor_1
-// this.motor_2
+// @param Object 'pins'
+//   pins.whisker_in
+//   pins.whisker_out
+//   pins.motor_1
+//   pins.motor_2
 var Motor = function(pins){
   this.pins = pins;
-
-  this.whisker = new Whisker({ pinIn : pins.whisker_in, out : pins.whisker_out });
 };
 
 Motor.prototype.enable = function(){
-  console.log("enabling whisker pins " + this.whisker.pinIn + " and " + this.whisker.out);
-  var whisker_in = this.whisker.pinIn;
-  gpio.open(whisker_in, "input", function(){});
-  
-  console.log(this.whisker.out);
-  var whisker_out = this.whisker.out;
-  gpio.open(whisker_out, "output", function(){
-    console.log(whisker_out);
-    gpio.write(whisker_out, HIGH, function(){});
-  });
-  
   console.log("enabling motor pins " + this.pins.motor_1 + " and " + this.pins.motor_2);
   gpio.open(this.pins.motor_1, "output", function(){})
   gpio.open(this.pins.motor_2, "output", function(){})
@@ -67,14 +65,20 @@ Motor.prototype.forward = function(){
 };
 
 // this commands and manages the motors in aggregate
-// @param Array 'motors'
+// @param Array 'motors' - array of Motor objects
+// @param Array 'whiskers' - array of Whisker objects
 //
-var KnightRider = function(motors){
-  this.motors = motors || []; 
+var KnightRider = function(motors, whiskers){
+  this.motors = motors || [];
+  this.whiskers = whiskers || []; 
 };
 
 KnightRider.prototype.addMotor = function(motor){
   this.motors.push(motor);
+};
+
+KnightRider.prototype.addWhisker = function(whisker){
+  this.whiskers.push(whisker);
 };
 
 KnightRider.prototype.forward = function(){
@@ -93,11 +97,14 @@ KnightRider.prototype.startEngine = function(){
   for (var i = 0, len = this.motors.length; i < len; i++){
     this.motors[i].enable();
   }
+  for (var i = 0, len = this.whiskers.length; i < len; i++){
+    this.whiskers[i].enable();
+  }
 };
 
 KnightRider.prototype.checkWhiskers = function(){
   var carBrain = this;
-  var whiskers = this.motors.map(function(motor){ return motor.whisker });
+  var whiskers = this.whiskers;
   console.log(whiskers);
 
   for (var i = 0, len = whiskers.length; i < len; i++){
@@ -110,6 +117,7 @@ KnightRider.prototype.checkWhiskers = function(){
 
 };
 
+console.log("creating MOTORS!");
 // motor factory class for spawning motors
 var MotorMaker = function(){
 };
@@ -143,8 +151,38 @@ var knightRider = new KnightRider();
 knightRider.addMotor(rightMotor);
 knightRider.addMotor(leftMotor);
 
-console.log("MOTORS!");
 console.log(knightRider.motors.length);
+
+console.log("creating WHISKERS!");
+// whisker factory class
+var WhiskerMaker = function(){
+};
+
+WhiskerMaker.prototype.whiskerClass = Whisker;
+
+WhiskerMaker.prototype.createWhisker = function(pins){
+  return new this.whiskerClass(pins);
+};
+
+var whiskerMaker = new WhiskerMaker();
+
+var leftWhiskerPins = {
+  pinIn : LEFT_WHISKER_IN,
+  out : LEFT_WHISKER_OUT
+};
+
+var rightWhiskerPins = {
+  pinIn : RIGHT_WHISKER_IN,
+  out : RIGHT_WHISKER_OUT
+};
+
+var rightWhisker = whiskerMaker.createWhisker(rightWhiskerPins);
+var leftWhisker = whiskerMaker.createWhisker(leftWhiskerPins);
+
+knightRider.addWhisker(rightWhisker);
+knightRider.addWhisker(leftWhisker);
+
+console.log(knightRider.whiskers.length);
 
 // this guy runs over and over in continuous loop
 var loop = function(){
