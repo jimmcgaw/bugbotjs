ec254254151c545716b0bdf300a254217f43e1b2
Jim McGaw <jpmcgaw@gmail.com>
2014-05-15 00:39:21 -0700

    vehicle class to daftbot

--- bugbot.js
+++ bugbot.js
@@ -25,168 +25,11 @@ var LEFT_MOTOR_1 = 7;
 var LEFT_MOTOR_2 = 15;
 
 
-// car brain - this commands and manages the motors in aggregate
-// @param Array 'motors' - array of Motor objects
-// @param Array 'whiskers' - array of Whisker objects
-//
-var KnightRider = function(motors, whiskers){
-  this.whiskers = whiskers || [];
-
-  this.leftMotor = motors.leftMotor;
-  this.rightMotor = motors.rightMotor;
-
-  this.motors = [];
-  this.addMotor(this.leftMotor);
-  this.addMotor(this.rightMotor);
-
-  this.state = "forward";
-};
-
-KnightRider.prototype.addMotor = function(motor){
-  this.motors.push(motor);
-};
-
-KnightRider.prototype.addWhisker = function(whisker){
-  this.whiskers.push(whisker);
-};
-
-KnightRider.prototype.forward = function(){
-  console.log("setting vehicle forward");
-  for (var i = 0, len = this.motors.length; i < len; i++){
-    this.motors[i].forward();
-  }
-};
-
-KnightRider.prototype.reverse = function(){
-  for (var i = 0, len = this.motors.length; i < len; i++){
-    this.motors[i].reverse();
-  }
-};
-
-KnightRider.prototype.halt = function(){
-  for (var i = 0, len = this.motors.length; i < len; i++){ 
-    this.motors[i].halt();
-  } 
-};
-
-KnightRider.prototype.right = function(){
-  this.halt();
-  this.leftMotor.forward();
-  this.rightMotor.reverse();
-};
-
-KnightRider.prototype.left = function(){
-  this.halt();
-  this.rightMotor.forward();
-  this.leftMotor.reverse();
-};
-
-KnightRider.prototype.rotate = function(){
-    this.left();
-};
-
-KnightRider.prototype.startEngine = function(){
-  for (var i = 0, len = this.motors.length; i < len; i++){
-    this.motors[i].enable();
-  }
-  for (var i = 0, len = this.whiskers.length; i < len; i++){
-    this.whiskers[i].enable();
-  }
-};
-
-
-KnightRider.prototype.checkWhiskers = function(){
-  // if this.state !== "forward"{
-  //   return;
-  // }
-
-  var carBrain = this;
-  var whiskers = this.whiskers;
-
-  for (var i = 0, len = whiskers.length; i < len; i++){
-    var whisker = whiskers[i];
-    console.log( "reading from pin " + whisker.pinIn );
-    gpio.read(whisker.pinIn, function(err, value){
-      console.log(value);
-      if (value === HIGH){
-        // we have a collision, reset
-        carBrain.reset();
-      }
-    });
-  }
-
-};
-
-// intiates a state sequence that resets the robot
-// to forward in a different direction.
-KnightRider.prototype.reset = function(){
-  var vehicle = this;
-
-  // stop robot immediately ( t = 0 seconds )
-  vehicle.state = "halt";
-
-  // t = 1 : go in reverse 
-  setTimeout(function(){
-    vehicle.state = "reverse";
-  }, 1000);
-
-  // t = 1.5 : stop again 
-  setTimeout(function(){
-    vehicle.state = "halt"
-  }, 1500);
-
-  // t = 2 : rotate
-  setTimeout(function(){
-    vehicle.state = "rotate"
-  }, 2000);
-
-  // t = 3.5 : stop again
-  setTimeout(function(){
-    vehicle.state = "halt"
-  }, 3500);
-
-  // t = 4: stop again
-  setTimeout(function(){
-    vehicle.state = "forward"
-  }, 4000);
-};
-
-// runs in loop; checks whiskers and turns robot if
-// we detect a whisker collision
-KnightRider.prototype.update = function(){
-
-  console.log("vehicle state: " + this.state);
-  // check object state
-  if (this.state === "forward"){
-    this.forward();
-    this.checkWhiskers();
-  } else if (this.state === "halt"){
-    this.halt();
-  } else if (this.state === "reverse") {
-    this.reverse();
-  } else if (this.state === "rotate") {
-    this.rotate();
-  }
-
-};
-
-
 
 // TODO: we can dispense with the factories,
 // they really aren't adding much value.
 // Either that or move into daftbot
 console.log("creating MOTORS!");
-// motor factory class for spawning motors
-var MotorMaker = function(){
-};
-
-MotorMaker.prototype.motorClass = daftbot.Motor;
-
-MotorMaker.prototype.createMotor = function(pins){
-  return new this.motorClass(pins);
-};
-
-var motorMaker = new MotorMaker();
 
 var leftMotorPins = {
   motor_1 : LEFT_MOTOR_1,
@@ -198,28 +41,17 @@ var rightMotorPins = {
   motor_2 : RIGHT_MOTOR_2
 };
 
-var rightMotor = motorMaker.createMotor(rightMotorPins);
-var leftMotor = motorMaker.createMotor(leftMotorPins);
+var rightMotor = new daftbot.Whisker(rightMotorPins);
+var leftMotor = new daftbot.Whisker(leftMotorPins);
 
-var knightRider = new KnightRider({
+var vehicle = new Vehicle({
   leftMotor : leftMotor,
   rightMotor : rightMotor
 });
 
-console.log(knightRider.motors.length);
+console.log(vehicle.motors.length);
 
 console.log("creating WHISKERS!");
-// whisker factory class
-var WhiskerMaker = function(){
-};
-
-WhiskerMaker.prototype.whiskerClass = daftbot.Whisker;
-
-WhiskerMaker.prototype.createWhisker = function(pins){
-  return new this.whiskerClass(pins);
-};
-
-var whiskerMaker = new WhiskerMaker();
 
 var leftWhiskerPins = {
   pinIn : LEFT_WHISKER_IN,
@@ -231,25 +63,25 @@ var rightWhiskerPins = {
   out : RIGHT_WHISKER_OUT
 };
 
-var rightWhisker = whiskerMaker.createWhisker(rightWhiskerPins);
-var leftWhisker = whiskerMaker.createWhisker(leftWhiskerPins);
+var rightWhisker = new daftbot.Whisker(rightWhiskerPins);
+var leftWhisker = new daftbot.Whisker(leftWhiskerPins);
 
-knightRider.addWhisker(rightWhisker);
-knightRider.addWhisker(leftWhisker);
+vehicle.addSensor(rightWhisker);
+vehicle.addSensor(leftWhisker);
 
-console.log(knightRider.whiskers.length);
+console.log(vehicle.whiskers.length);
 
 // this guy runs over and over in continuous loop
 var loop = function(){
-  knightRider.update(); 
+  vehicle.update(); 
 };
 
 var setup = function(){
   console.log("starting engine");
-  knightRider.startEngine();  // enable motor and whisker pins
+  vehicle.startEngine();  // enable motor and whisker pins
 
   console.log("forward");
-  knightRider.forward();  //tallyho!
+  vehicle.forward();  //tallyho!
   console.log("running loop");
   setInterval(loop, INTERVAL);
 };
